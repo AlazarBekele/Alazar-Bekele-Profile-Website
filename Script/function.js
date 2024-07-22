4d583f8b4ee6e19cca068ecdb5f81d8c0d8e9da7
Alazar Bekele <152716794+AlazarBekele@users.noreply.github.com>
2024-07-22 17:49:53 +0300

    Add files via upload

---
+++ Script/function.js
@@ -0,0 +1,17 @@
+
+        function OnClickDouwnBtn () {
+            let IncreaseWidth = document.getElementById('IncrWid').classList = "IncremanetWidth"
+        console.log("It's Work");
+}
+
+    function Load () {
+        const Loading = document.getElementById ("LoadMainImg").classList = "NewMainClassImg"
+        const DivLoad = document.getElementById ("divMain").classList = "NewDivMainClass"
+    }
+            setTimeout (Load, 6000);
+
+    function Qrcode () {
+        const QrThree = document.getElementById ("QrThree").classList.add = "OnqrCLick"
+        const QrTwo = document.getElementById ("QrTwo").classList.add = "OnqrCLickTwo"
+        const QrOne = document.getElementById ("QrOne").classList.add = "OnqrCLickOne"
+}
